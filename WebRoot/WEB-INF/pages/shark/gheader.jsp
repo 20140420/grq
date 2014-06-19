922b531b61b23e80560a451683c93af9b2440135
姜泉 <595121869@qq.com>
2014-06-19 20:46:52 +0800

    Customer拦截器配置
    Customer拦截器配置

--- WebRoot/WEB-INF/pages/shark/gheader.jsp
+++ WebRoot/WEB-INF/pages/shark/gheader.jsp
@@ -1,14 +1,12 @@
 <%@ page contentType="text/html; charset=utf-8" language="java" import="java.sql.*" errorPage="" %>
 <%@ taglib prefix="s" uri="/struts-tags"%>
 
-
-<s:url action="manage_manage" namespace="/pages" var="manage_manage"></s:url>
 <s:set var="context_path" value="#request.get('javax.servlet.forward.context_path')"></s:set>
 
 <div>
 	<ul class="header-menu">
 	 <li><s:a action="indexweb" namespace="/" target="_blank">主页</s:a></li>
-	 <li><s:a action="manage_manage" namespace="/manage" target="_blank">资金</s:a></li>
+	 <li><s:a action="usidebar_uinquiryaccount" namespace="/user" target="mdetail">资金</s:a></li>
 	 <li><s:a action="invest_ranking" namespace="/invest" target="_blank">排名</s:a></li>
 	 <li><s:a action="shark_grule" namespace="/shark" target="_blank">规则</s:a></li>
 	</ul>
---
+++ src/com/grq/controller/action/interceptor/CustomerLoginInteceptor.java
@@ -0,0 +1,28 @@
+package com.grq.controller.action.interceptor;
+
+import java.util.Map;
+
+import com.grq.controller.action.BaseAction;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+/**
+ * 消费者登录拦截器
+ * @author JiangQuan
+ *
+ */
+public class CustomerLoginInteceptor extends AbstractInterceptor{
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 拦截器
+	 */
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		ActionContext context = invocation.getInvocationContext();// 获取ActionContext
+		Map<String, Object> session = context.getSession();// 获取Map类型的session
+		if(session.get("customer") != null){// 判断用户是否登录 
+			return invocation.invoke();// 调用执行方法
+		}
+		return BaseAction.CUSTOMER_LOGIN;// 返回登录
+	}
+}
