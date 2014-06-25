7f5df2c5f258de51d9dd91981888050911ca794c
姜泉 <595121869@qq.com>
2014-06-25 17:29:49 +0800

    键盘panel信息的pojo以及hibernate对应未用，pojo待具体设计
    键盘panel信息的pojo以及hibernate对应未用，pojo待具体设计

---
+++ src/com/grq/model/pojo/shark/PanelInfo.java
@@ -0,0 +1,12 @@
+package com.grq.model.pojo.shark;
+
+import java.io.Serializable;
+
+/**
+ * 键盘信息
+ * @author JiangQuan
+ */
+public class PanelInfo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+}
