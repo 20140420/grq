7247e990980feb23b5c7b7e0bab9c24d57990daa
姜泉 <595121869@qq.com>
2014-07-04 17:02:47 +0800

    之后再明确局部全局变量
    之后再明确局部全局变量

--- src/com/grq/controller/action/shark/sharkTest.java
+++ src/com/grq/controller/action/shark/sharkTest.java
@@ -32,7 +32,7 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 	static int timesRabbit = 6;
 	
 	//配置时间
-	int time_spend= 3;//计算转盘花费的时间4秒，从而计算转盘速度
+	int time_circle= 3;//计算转盘花费的时间4秒，从而计算转盘速度
 	int time_observe= 6; //观察时长 后期由服务器取得
 	int time_bet= 12;//下注时长 后期由服务器取得
 	int time_display= 3;//展示时长 后期由服务器取得
@@ -42,15 +42,14 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 	int circle_num= 2;//转盘圈数 后期由服务器取得
 	//关键配置
 	static int timesMax = 99;//最高倍数
-	static double commission_rate = 0.10; //佣金费率
-	double single_limit = 1000.00; //单注上限
+	static double commission_rate = 0.60; //佣金费率
+	//int single_bet = 1; //单注额度调整
 	int bet_limit = 999; //押注上限
-	
+	double initial_dividend =300;//彩金初始值
 	
 	//本类中的区域变量
 	static double dividend; //彩金池变量需要储存在数据库中
 	private static Random randomNum = new Random();//用于获取随机数
-	private static double totalPriceSum; //统计下注总额之和的变量
 	private static double totalSwallowSum; //统计燕子下注总额之和
 	private static double totalPigeonSum;
 	private static double totalPeafowlSum;
@@ -68,20 +67,24 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 	private static Prize prizeString;//奖项变量，默认正在抽奖
 	
 	private static boolean bombOrNot; //是否存在炸弹，默认否
-	// 奖项列表
+	// 奖项实体列表
 	private static List<PrizeRecord> prizelist;
-	private static Integer totalBet;//下注总筹码
-	private static double totalPrice;//下注总金额
+	private static Integer totalBet;//一条下注订单总筹码
+	private static double totalBetSum;//一场下注总金额
+	
+	
 	
 	
 	public static void main(String[] args) {
-		List<PrizeRecord> beforePrizeList = beforePrizeList();
+		List<PrizeRecord> beforePrizeList = beforePrize();
 		dividend = beforePrizeList.get(0).getDividend();//获取上一盘彩金池
+		
 		System.out.println("上一期彩金："+dividend);
 		prizeString = havePrize();
-		System.out.println("单场押注总额之和为："+totalPriceSum);
+		System.out.println("单场押注总额之和为："+totalBetSum);
+		
 		System.out.println("系统费率："+commission_rate);
-		System.out.println("奖项为:"+prizeString);
+		System.out.println("本场中奖奖项为:"+prizeString);
 		if(againOrNot == true){//如果是则获得再转一次
 			turnAgain();
 			System.out.println("本奖项有重转。");
@@ -92,7 +95,11 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 	private static void turnAgain() {
 		System.out.println("再转函数");		
 	}
-	private static List<PrizeRecord> beforePrizeList() {
+	/**
+	 * 上一场的奖项记录
+	 * @return
+	 */
+	private static List<PrizeRecord> beforePrize() {
 		/*String field =" dividend,prize_name ";//选择字段奖项
 		String where = null;
 		Object[] queryParams = null;
@@ -103,14 +110,14 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 		//添加数据测试
 		prizelist=new ArrayList<PrizeRecord>();		
 		PrizeRecord prize1 = new PrizeRecord();	
-		prize1.setDividend(3000.0);
+		prize1.setDividend(300.0);
 		prize1.setPrizeName(Prize.RABBIT);
 		prizelist.add(prize1);		
 		PrizeRecord prize2 = new PrizeRecord();
 		prize2.setDividend(320.0);
 		prize2.setPrizeName(Prize.PIGEON);
 		prizelist.add(prize2);
-		
+
 		return prizelist;
 	}
 	/**
@@ -135,7 +142,7 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 	private static Prize dividendUpPrize() {
 		System.out.print("吃分函数：");
 		againOrNot = false; //设置为false不重转
-		double priceForPrize = totalPriceSum*(1-commission_rate);//单场用于发奖注额
+		double priceForPrize = totalBetSum*(1-commission_rate);//单场用于发奖注额
 		double swallowOutScore = totalSwallowSum*timesSwallow;
 		double pigeonOutScore = totalPigeonSum*timesPigeon;
 		double peafowlOutScore = totalPeafowlSum*timesPeafowl;
@@ -144,53 +151,53 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 		double pandaOutScore = totalPandaSum*timesPanda;
 		double monkeyOutScore = totalMonkeySum*timesMonkey;
 		double rabbitOutScore = totalRabbitSum*timesRabbit;		
-		ArrayList<Object> totalSum = new ArrayList<Object>();//八个奖出分集合		
-		totalSum.add(swallowOutScore);//注意顺序不能弄乱
-		totalSum.add(pigeonOutScore);
-		totalSum.add(peafowlOutScore);
-		totalSum.add(eagleOutScore);
-		totalSum.add(lionOutScore);
-		totalSum.add(pandaOutScore);
-		totalSum.add(monkeyOutScore);
-		totalSum.add(rabbitOutScore);
+		ArrayList<Object> totalOutSum = new ArrayList<Object>();//八个奖出分集合		
+		totalOutSum.add(swallowOutScore);//注意顺序不能弄乱
+		totalOutSum.add(pigeonOutScore);
+		totalOutSum.add(peafowlOutScore);
+		totalOutSum.add(eagleOutScore);
+		totalOutSum.add(lionOutScore);
+		totalOutSum.add(pandaOutScore);
+		totalOutSum.add(monkeyOutScore);
+		totalOutSum.add(rabbitOutScore);
+		System.out.println("八个奖出分集合totalSum值："+totalOutSum);
 		double birdOutScore = getTotalBirdSum()*2;//飞禽总出分 ;
 		double beastOutScore = getTotalBeastSum()*2;//走兽总出分 ;
-		Vector<Object> less = lessThan(totalSum,birdOutScore,beastOutScore,priceForPrize);//获得能吃分的几个奖项的索引的集合
+		Vector<Object> less = lessThan(totalOutSum,birdOutScore,beastOutScore,priceForPrize);//获得能吃分的几个奖项的索引的集合
 		if(less.size() > 0 && less != null){//如果存在能吃分的奖项的索引
-			System.out.println("totalSum值："+totalSum);
 			System.out.println("存在正常吃分奖项");
 			int index=(int)(Math.random()*less.size());//从总共less.size()个能吃分奖中，随机一个能吃分奖的索引的索引，
 			if((Integer)less.get(index) == 0){//第index个吃分奖对应的索引
 				dividend= dividend -swallowOutScore-birdOutScore;
-				System.out.println("燕子发奖："+totalSum.get((Integer) less.get(index)));
+				System.out.println("燕子发奖："+totalOutSum.get((Integer) less.get(index)));
 				return Prize.SWALLOW;
 			} else if((Integer)less.get(index) == 1){
 				dividend= dividend -pigeonOutScore-birdOutScore;
-				System.out.println("鸽子发奖："+totalSum.get((Integer) less.get(index)));
+				System.out.println("鸽子发奖："+totalOutSum.get((Integer) less.get(index)));
 				return Prize.PIGEON;
 			} else if((Integer)less.get(index) == 2){
 				dividend= dividend -peafowlOutScore-birdOutScore;
-				System.out.println("孔雀发奖："+totalSum.get((Integer) less.get(index)));
+				System.out.println("孔雀发奖："+totalOutSum.get((Integer) less.get(index)));
 				return Prize.PEAFOWL;
 			} else if((Integer)less.get(index) == 3){
 				dividend= dividend -eagleOutScore-birdOutScore;
-				System.out.println("老鹰发奖："+totalSum.get((Integer) less.get(index)));
+				System.out.println("老鹰发奖："+totalOutSum.get((Integer) less.get(index)));
 				return Prize.EAGLE;
 			} else if((Integer)less.get(index) == 4){
 				dividend= dividend -lionOutScore-beastOutScore;
-				System.out.println("狮子发奖："+totalSum.get((Integer) less.get(index)));
+				System.out.println("狮子发奖："+totalOutSum.get((Integer) less.get(index)));
 				return Prize.LION; 
 			} else if((Integer)less.get(index) == 5){
 				dividend= dividend -pandaOutScore-beastOutScore;
-				System.out.println("熊猫发奖："+totalSum.get((Integer) less.get(index)));
+				System.out.println("熊猫发奖："+totalOutSum.get((Integer) less.get(index)));
 				return Prize.PANDA;
 			}  else if((Integer)less.get(index) == 6){
 				dividend= dividend -monkeyOutScore-beastOutScore;
-				System.out.println("猴子发奖："+totalSum.get((Integer) less.get(index)));
+				System.out.println("猴子发奖："+totalOutSum.get((Integer) less.get(index)));
 				return Prize.MONKEY;
 			}  else if((Integer)less.get(index) == 7){
 				dividend= dividend -rabbitOutScore-beastOutScore;
-				System.out.println("兔子发奖："+totalSum.get((Integer) less.get(index)));
+				System.out.println("兔子发奖："+totalOutSum.get((Integer) less.get(index)));
 				return Prize.RABBIT;
 			} else{
 				System.out.print("不应该出现的error");
@@ -200,37 +207,37 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 			int countNum = 20 ;//统计20场			
 			bombOrNot = queryNumResult(countNum);//查看20场内是否存在地雷
 			if( bombOrNot == true){//如果20场内出现地雷奖项
-				int i = outScoreMin(totalSum,birdOutScore,beastOutScore);//获取符合要求最小出分的奖项的索引
+				int i = outScoreMin(totalOutSum,birdOutScore,beastOutScore);//获取符合要求最小出分的奖项的索引
 				if(i == 0){//第一个奖对应的索引
-					System.out.println("燕子发奖："+totalSum.get(i));
+					System.out.println("燕子发奖："+totalOutSum.get(i));
 					dividend= dividend -swallowOutScore-birdOutScore;
 					return Prize.SWALLOW;
 				} else if(i == 1){
-					System.out.println("鸽子发奖："+totalSum.get(i));
+					System.out.println("鸽子发奖："+totalOutSum.get(i));
 					dividend= dividend -pigeonOutScore-birdOutScore;
 					return Prize.PIGEON;
 				} else if(i == 2){
-					System.out.println("孔雀发奖："+totalSum.get(i));
+					System.out.println("孔雀发奖："+totalOutSum.get(i));
 					dividend= dividend -peafowlOutScore-birdOutScore;
 					return Prize.PEAFOWL;
 				} else if(i == 3){
-					System.out.println("老鹰发奖："+totalSum.get(i));
+					System.out.println("老鹰发奖："+totalOutSum.get(i));
 					dividend= dividend -eagleOutScore-birdOutScore;
 					return Prize.EAGLE;
 				} else if(i == 4){
-					System.out.println("狮子发奖："+totalSum.get(i));
+					System.out.println("狮子发奖："+totalOutSum.get(i));
 					dividend= dividend -lionOutScore-beastOutScore;
 					return Prize.LION; 
 				} else if(i == 5){
-					System.out.println("熊猫发奖："+totalSum.get(i));
+					System.out.println("熊猫发奖："+totalOutSum.get(i));
 					dividend= dividend -pandaOutScore-beastOutScore;
 					return Prize.PANDA;
 				}  else if(i == 6){
-					System.out.println("猴子发奖："+totalSum.get(i));
+					System.out.println("猴子发奖："+totalOutSum.get(i));
 					dividend= dividend -monkeyOutScore-beastOutScore;
 					return Prize.MONKEY;
 				}  else if(i == 7){
-					System.out.println("兔子发奖："+totalSum.get(i));
+					System.out.println("兔子发奖："+totalOutSum.get(i));
 					dividend= dividend -rabbitOutScore-beastOutScore;
 					return Prize.RABBIT;
 				} else{
@@ -264,9 +271,12 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 		PrizeRecord prize2 = new PrizeRecord();
 		prize2.setPrizeName(Prize.BOMB);
 		prizelist.add(prize2);
-		
+		ArrayList<Object> onlyPrizeList = new ArrayList<Object>();//之前中奖列表
+		for(PrizeRecord prizeRecord: prizelist){
+			onlyPrizeList.add(prizeRecord.getPrizeName());
+		}
+		System.out.println("query之前中奖列表："+onlyPrizeList);
 		for(int i=0 ; i < prizelist.size() ;i++){
-			System.out.println(prizelist.get(i).getPrizeName());
 			if(prizelist.get(i).getPrizeName() == Prize.BOMB){
 				return true;//如果遍历的第一个就是地雷，后面19个就不遍历了
 			}
@@ -301,16 +311,16 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
         }
 		return 0;
 	}
-	private static Vector<Object> lessThan(ArrayList<Object> totalSum,
+	private static Vector<Object> lessThan(ArrayList<Object> totalOutSum,
 			double birdOutScore, double beastOutScore, double priceForPrize) {
-		Vector<Object> v = new Vector<Object>(totalSum.size());
-		for(int i=0 ; i<totalSum.size() ; i++){
-			if( i < (totalSum.size()/2) ){//飞禽类
-				if ((Double)totalSum.get(i) < (priceForPrize - birdOutScore)){
+		Vector<Object> v = new Vector<Object>(totalOutSum.size());
+		for(int i=0 ; i<totalOutSum.size() ; i++){
+			if( i < (totalOutSum.size()/2) ){//飞禽类
+				if ((Double)totalOutSum.get(i) < (priceForPrize - birdOutScore)){
 					v.add(i);
 				}
 			} else {//走兽类
-				if ((Double)totalSum.get(i) < (priceForPrize - beastOutScore)){
+				if ((Double)totalOutSum.get(i) < (priceForPrize - beastOutScore)){
 					v.add(i);
 				}
 			}
@@ -322,8 +332,8 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 		try{
 			if( totalBombSum > 0 ){//如果有人下注
 				System.out.println("炸有人:" + totalBombSum);
-				dividend= dividend -totalPriceSum*(1-commission_rate);
-				System.out.println("发炸弹金额:" + totalPriceSum*(1-commission_rate));
+				dividend= dividend -totalBetSum*(1-commission_rate);
+				System.out.println("发炸弹金额:" + totalBetSum*(1-commission_rate));
 				return Prize.BOMB;
 			} else {
 				System.out.println("炸弹无人:" + totalBombSum);
@@ -405,7 +415,7 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 			double goldOutScore = getTotalGoldSum()*goldSharkTimes;//金鲨总出分
 			dividend = dividend - goldOutScore; //发了金鲨奖的彩金池
 			//记得更新到数据库
-			System.out.print("金鲨倍数："+goldSharkTimes);
+			System.out.println("金鲨倍数："+goldSharkTimes);
 			return Prize.GOLD_SHARK;//出奖金鲨+再转一次
 		} else {
 			System.out.print("不应该出现的error");
@@ -472,7 +482,7 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 		
 		
 		for(PanelInfo panelInfo : allBet){//遍历所有的下注条目
-			float single_bet = panelInfo.getSingle_bet();//获取每一条单注额度
+			//float single_bet = panelInfo.getSingle_bet();//获取每一条单注额度
 			Integer swallow = panelInfo.getSwallow();
 			Integer pigeon = panelInfo.getPigeon();
 			Integer peafowl = panelInfo.getPeafowl();
@@ -488,43 +498,29 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 			Integer beast = panelInfo.getBeast();
 			boolean betCount = panelInfo.getBetCount();//获得条目统计状态
 			if(betCount == false){//如果条目未操作过
-				float totalSwallow = swallow*single_bet;
-				totalSwallowSum += totalSwallow;
-				float totalPigeon = pigeon*single_bet;
-				totalPigeonSum += totalPigeon;//在totalPigeonSum不要求为静态等同于setTotalPigeonSum(getTotalPigeonSum() + totalPigeon)
-				float totalPeafowl = peafowl*single_bet;
-				totalPeafowlSum += totalPeafowl;
-				float totalEagle = eagle*single_bet;
-				totalEagleSum += totalEagle;
-				float totalLion = lion*single_bet;
-				totalLionSum += totalLion;
-				float totalPanda = panda*single_bet;
-				totalPandaSum += totalPanda;
-				float totalMonkey = monkey*single_bet;
-				totalMonkeySum += totalMonkey;
-				float totalRabbit = rabbit*single_bet;
-				totalRabbitSum += totalRabbit;
-				float totalBird = bird*single_bet;
-				totalBirdSum += totalBird;
-				float totalSilver = silver*single_bet;
-				totalSilverSum += totalSilver;
-				float totalBomb = bomb*single_bet;
-				totalBombSum += totalBomb;
-				float totalGold = gold*single_bet;
-				totalGoldSum += totalGold;
-				float totalBeast = beast*single_bet;
-				totalBeastSum += totalBeast;//相加所有条目禽兽的下注总额
-				totalBet = (swallow+pigeon+peafowl+eagle+lion+panda+monkey+rabbit+bird+silver+bomb+gold+beast);
-				totalPrice = single_bet*totalBet;
-				totalPriceSum += totalPrice;//相加所有条目的下注总额
+				totalSwallowSum += swallow;
+				totalPigeonSum += pigeon;//在totalPigeonSum不要求为静态等同于setTotalPigeonSum(getTotalPigeonSum() + totalPigeon)
+				totalPeafowlSum += peafowl;
+				totalEagleSum += eagle;
+				totalLionSum += lion;
+				totalPandaSum += panda;
+				totalMonkeySum += monkey;
+				totalRabbitSum += rabbit;
+				totalBirdSum += bird;
+				totalSilverSum += silver;
+				totalBombSum += silver;
+				totalGoldSum += gold;
+				totalBeastSum += beast;//相加所有条目禽兽的下注总额
+				totalBet = (swallow+pigeon+peafowl+eagle+lion+panda+monkey+rabbit+bird+silver+bomb+gold+beast);//一条订单总下注
+				setTotalBetSum(getTotalBetSum() + totalBet);//相加所有条目的下注总额
 				betCount = true; //设置改变标记为已经统计过
 				//panelData = sharkDaoTest.load(panelData.getPanelBetId());//装载订单对象
 				panelData.setBetCount(betCount);// 设置操作情况
 				//sharkDaoTest.update(panelData);//更新修改操作状态
 			}
 		}
-		System.out.println("系统单场押注总额之和为："+totalPriceSum);
-		return totalPriceSum;//返回总下注数目之和
+		System.out.println("系统单场押注总额之和为："+totalBetSum);
+		return totalBetSum;//返回总下注数目之和
 	}
 
 	// 键盘panel对象
@@ -545,12 +541,6 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 	//	sharkTest.pageModel = pageModel;
 	//}
 	//getter和setter方法
-	public double getTotalPriceSum() {
-		return totalPriceSum;
-	}
-	public void setTotalPriceSum(float totalPriceSum) {
-		sharkTest.totalPriceSum = totalPriceSum;
-	}
 	public static double getTotalSwallowSum() {
 		return totalSwallowSum;
 	}
@@ -629,16 +619,16 @@ public class sharkTest extends BaseAction implements ModelDriven<PanelInfo>{
 	public void setTotalBeastSum(float totalBeastSum) {
 		sharkTest.totalBeastSum = totalBeastSum;
 	}
-	public boolean isAgainOrNot() {
-		return againOrNot;
-	}
-	public void setAgainOrNot(boolean againOrNot) {
-		sharkTest.againOrNot = againOrNot;
-	}
 	public static List<PrizeRecord> getPrizelist() {
 		return prizelist;
 	}
 	public static void setPrizelist(List<PrizeRecord> prizelist) {
 		sharkTest.prizelist = prizelist;
+	}
+	public static double getTotalBetSum() {
+		return totalBetSum;
+	}
+	public static void setTotalBetSum(double totalBetSum) {
+		sharkTest.totalBetSum = totalBetSum;
 	}
 }
