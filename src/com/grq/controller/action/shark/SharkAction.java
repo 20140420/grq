730411abc277d7530eae53102ff09119467a82fb
姜泉 <595121869@qq.com>
2014-07-05 21:11:38 +0800

    有问题，还行
    问题一：下过一次，第二次没下还是有，需要写betClear()函数完成重置清零
    问题二：出奖统计下单时当数据库中的下单都是统计过时，即没有新下单时会出错，这个需要解决

--- src/com/grq/controller/action/shark/SharkAction.java
+++ src/com/grq/controller/action/shark/SharkAction.java
@@ -119,6 +119,185 @@ public class SharkAction extends BaseAction implements ModelDriven<PanelInfo>{
 		return MAIN;//返回主题页	
 	}
 	
+
+	
+/**-------------------------------------------------------------------*/
+	/**
+	 * 从panel提交获得数据,
+	 * @return shark页面
+	 * @throws Exception
+	 */
+	public String save() throws Exception{
+		//System.out.print("panel提交的data");
+		if(getLoginCustomer() != null){//如果用户已登录
+			//System.out.print("用户登入的");			
+			Integer swallow =panelData.getSwallow();// 燕子
+			Integer pigeon =panelData.getPigeon();// 鸽子
+			Integer peafowl =panelData.getPeafowl();// 孔雀
+			Integer eagle =panelData.getEagle();// 老鹰
+			Integer lion =panelData.getLion();// 狮子
+			Integer panda =panelData.getPanda();// 熊猫
+			Integer monkey =panelData.getMonkey();// 猴子
+			Integer rabbit =panelData.getRabbit();// 兔子
+			Integer bird =panelData.getBird();// 飞禽
+			Integer silver_shark =panelData.getSilver_shark();// 银鲨
+			Integer bomb =panelData.getBomb();// 炸弹
+			Integer gold_shark =panelData.getGold_shark();// 金鲨
+			Integer beast =panelData.getBeast();// 走兽
+			Integer totalBet =0;// 计算下注总数（非总额）的变量
+			totalBet=(swallow+pigeon+peafowl+eagle+lion+panda+monkey+rabbit+bird+silver_shark+bomb+gold_shark+beast);
+			//System.out.print("下注总数："+totalBet);
+			if (totalBet != 0){//如果有下注
+				Integer single_bet=panelData.getSingle_bet(); //单注额度
+				float totalPrice = 0f; // 计算总额的变量
+				totalPrice =single_bet*totalBet;
+				System.out.println("save总额："+totalPrice);
+				Boolean betCount = false;// 用于判断是否是统计过（默认值为false）
+				panelData.setPanelBetId(StringUtil.createOrderId());// 设置21位的订单号
+				panelData.setCustomer(getLoginCustomer());// 设置所属用户	
+				panelData.setSwallow(swallow);
+				panelData.setPigeon(pigeon);
+				panelData.setPeafowl(peafowl);
+				panelData.setEagle(eagle);
+				panelData.setLion(lion);
+				panelData.setPanda(panda);
+				panelData.setMonkey(monkey);
+				panelData.setRabbit(rabbit);
+				panelData.setBird(bird);
+				panelData.setSilver_shark(silver_shark);
+				panelData.setBomb(bomb);
+				panelData.setGold_shark(gold_shark);
+				panelData.setBeast(beast);
+				panelData.setTotalBet(totalBet);//把下注总数加到对象panelData中
+				panelData.setSingle_bet(single_bet);
+				panelData.setTotalPrice(totalPrice);//把下注总额度
+				panelData.setBetCount(betCount);// 用于判断是否是统计过（默认值为false）
+				panelData.setPrizeItem(Prize.RAFFLING);// 设置奖项状态为正在抽奖
+				betDao.save(panelData);//保存panel获得数据	
+				//记得重置panel下注
+			}
+			//return MAIN;//返回shark主页面
+			return main();//通过主函数返回shark主页面，可进行统计出结果
+		}
+		return CUSTOMER_LOGIN;//返回登入页面
+	}
+	public String uinquirybet() throws Exception{
+
+		return findByCustomer();//返回查询个人下注的方法
+	}
+	/**
+	 * 查询个人下注
+	 * @return String
+	 * @throws Exception
+	 */
+	public String findByCustomer() throws Exception {
+		if(getLoginCustomer() != null){//如果用户已登录
+			String where = "where customer.id = ?";//将用户id设置为查询条件
+			Object[] queryParams = {getLoginCustomer().getId()};//创建对象数组
+			Map<String, String> orderby = new HashMap<String, String>(1);//创建Map集合
+			orderby.put("createTime", "desc");//设置排序条件及方式
+			pageModel = betDao.find(where, queryParams, orderby , pageNo, pageSize);//执行查询方法
+		}
+		return LIST;//返回订单列表页面
+	}
+	
+	/**
+	 * 查询所有人下注
+	 * @return String
+	 * @throws Exception
+	 */
+	public String list() throws Exception {
+		Map<String, String> orderby = new HashMap<String, String>(1);//定义Map集合
+		orderby.put("createTime", "desc");//设置按创建时间倒序排列
+		StringBuffer whereBuffer = new StringBuffer("");//创建字符串对象
+		List<Object> params = new ArrayList<Object>();//创建列表集合
+		if(panelData.getPanelBetId() != null && panelData.getPanelBetId().length() > 0){//如果下注单不为空
+			whereBuffer.append("panelBetId = ?");//以下注单号为查询条件
+			params.add(panelData.getPanelBetId());//设置参数
+		}
+		if(panelData.getCustomer() != null && panelData.getCustomer().getUsername() != null 
+				&& panelData.getCustomer().getUsername().length() > 0){//如果会员名不为空
+			if(params.size() > 0) whereBuffer.append(" and ");//增加查询条件
+			whereBuffer.append("customer.username = ?");//设置会员名为查询条件
+			params.add(panelData.getCustomer().getUsername());//设置参数
+		}
+		//如果whereBuffer为空则查询条件为空，否则以whereBuffer为查询条件
+		String where = whereBuffer.length()>0 ? "where "+whereBuffer.toString() : "";
+		pageModel = betDao.find(where, params.toArray(), orderby, pageNo, pageSize);//执行查询方法
+		System.out.println("最新一单飞禽押注："+pageModel.getList().get(0).getBird());
+		return LIST;//返回后台下注列表
+	}
+	
+	/**
+	 * 查询指定一单的下注情况
+	 */
+	public String select() throws Exception {
+		panelData = betDao.load(panelData.getPanelBetId());
+		return SELECT;
+	}
+	
+	
+	/**
+	 * 转盘页面
+	 * @return
+	 * @throws Exception
+	 */
+	public String gdial() throws Exception{
+
+		return "gdial";
+	}
+	
+	/**
+	 * 按键键盘页面
+	 * @return
+	 * @throws Exception
+	 */
+	public String gpanel() throws Exception{
+
+		return "gpanel";
+	}
+	
+	/**
+	 * 导向grule页面
+	 * @return
+	 * @throws Exception
+	 */
+	public String grule() throws Exception{
+
+		return "grule";
+	}
+	/**
+	 * 游戏shark的设定
+	 * @return shark设定页面
+	 * @throws Exception
+	 */
+	public String option() throws Exception{
+
+		return "option";
+	}
+	
+	// 键盘panel对象
+	private PanelInfo panelData = new PanelInfo();//一定要先初始化obj对象！
+	private PageModel<PanelInfo> pageModel;// 分页组件
+	/**
+	 * 此方法是Modeldriven接口的方法，要重写
+	 * @return
+	 */
+	@Override
+	public PanelInfo getModel() {
+		return panelData;
+	}
+	//getter和setter方法，放入request中，好在jsp页面中能拿到	
+	public PageModel<PanelInfo> getPageModel() {
+		return pageModel;
+	}
+
+	public void setPageModel(PageModel<PanelInfo> pageModel) {
+		this.pageModel = pageModel;
+	}
+/**---------------------------------------------------------------------*/
+	
+	
 	/**
 	 * 获取倍数
 	 * @return
@@ -268,7 +447,7 @@ public class SharkAction extends BaseAction implements ModelDriven<PanelInfo>{
 	 * 出奖
 	 * @return
 	 */
-	private static Prize havePrize(){
+	private Prize havePrize(){
 		double totalPriceSum = countTotalPrice();//获得下注总额之和
 		dividend = lastDividend + totalPriceSum*(1-commission_rate);//筹码注入彩金池
 		commissionProfit = totalPriceSum*commission_rate;//佣金收益
@@ -288,59 +467,17 @@ public class SharkAction extends BaseAction implements ModelDriven<PanelInfo>{
 	 * 统计各奖项押注之和，及总押注总额之和
 	 * @return float 未操作押注总额之和 totalBetSum
 	 */
-	private static double countTotalPrice() {
-		/*
+	private double countTotalPrice() {
+		
 		Map<String, String> orderby = new HashMap<String, String>(1);//定义Map集合
 		orderby.put("createTime", "desc");//设置按创建时间倒序排列
 		String where = "where betCount = ?";//设置查询条件语句
 		Object[] queryParams = {false};//获取未操作过的参数值
-		pageModel = betDaoTest.find(where, queryParams, orderby, -1, -1);//执行查询方法
-		System.out.println("最新一单飞禽押注："+pageModel.getList().get(0).getBird());
-		List<PanelInfo> allBet = pageModel.getList();//获取所有未操作过的下注条目
-		*/
+		pageModel = betDao.find(where, queryParams, orderby, -1, -1);//执行查询方法
+		System.out.println("上一场飞禽押注（非本场）应该是最近一条下单："+pageModel.getList().get(0).getBird());
+		betEntity = pageModel.getList();//获取所有未操作过的下注条目
 		
 		//添加数据测试
-		betEntity = new ArrayList<PanelInfo>();		
-		PanelInfo bet1 = new PanelInfo();
-		bet1.setSingle_bet(10);
-		bet1.setSwallow(1);
-		bet1.setPigeon(1);
-		bet1.setPeafowl(1);
-		bet1.setEagle(1);
-		bet1.setLion(1);
-		bet1.setPanda(1);
-		bet1.setMonkey(1);
-		bet1.setRabbit(1);
-		bet1.setBird(1);
-		bet1.setBeast(1);
-		bet1.setBomb(1);
-		bet1.setSilver_shark(1);
-		bet1.setGold_shark(1);
-		bet1.setCreateTime(null);	
-		bet1.setPrizeItem(Prize.RAFFLING);
-		bet1.setBetCount(false);
-		betEntity.add(bet1);
-		
-		PanelInfo bet2 = new PanelInfo();
-		bet2.setSingle_bet(10);
-		bet2.setSwallow(1);
-		bet2.setPigeon(1);
-		bet2.setPeafowl(1);
-		bet2.setEagle(1);
-		bet2.setLion(1);
-		bet2.setPanda(1);
-		bet2.setMonkey(1);
-		bet2.setRabbit(1);
-		bet2.setBird(1);
-		bet2.setBeast(1);
-		bet2.setBomb(0);
-		bet2.setSilver_shark(1);
-		bet2.setGold_shark(1);
-		bet2.setCreateTime(null);	
-		bet2.setPrizeItem(Prize.RAFFLING);
-		bet2.setBetCount(false);
-		betEntity.add(bet2);
-		
 		
 		for(PanelInfo panelInfo : betEntity){//遍历所有的下注条目
 			//float single_bet = panelInfo.getSingle_bet();//获取每一条单注额度
@@ -692,181 +829,5 @@ public class SharkAction extends BaseAction implements ModelDriven<PanelInfo>{
 		System.out.println("再转函数");		
 	}
 	
-	
-/**-------------------------------------------------------------------*/
-	/**
-	 * 从panel提交获得数据,
-	 * @return shark页面
-	 * @throws Exception
-	 */
-	public String save() throws Exception{
-		//System.out.print("panel提交的data");
-		if(getLoginCustomer() != null){//如果用户已登录
-			//System.out.print("用户登入的");			
-			Integer swallow =panelData.getSwallow();// 燕子
-			Integer pigeon =panelData.getPigeon();// 鸽子
-			Integer peafowl =panelData.getPeafowl();// 孔雀
-			Integer eagle =panelData.getEagle();// 老鹰
-			Integer lion =panelData.getLion();// 狮子
-			Integer panda =panelData.getPanda();// 熊猫
-			Integer monkey =panelData.getMonkey();// 猴子
-			Integer rabbit =panelData.getRabbit();// 兔子
-			Integer bird =panelData.getBird();// 飞禽
-			Integer silver_shark =panelData.getSilver_shark();// 银鲨
-			Integer bomb =panelData.getBomb();// 炸弹
-			Integer gold_shark =panelData.getGold_shark();// 金鲨
-			Integer beast =panelData.getBeast();// 走兽
-			Integer totalBet =0;// 计算下注总数（非总额）的变量
-			totalBet=(swallow+pigeon+peafowl+eagle+lion+panda+monkey+rabbit+bird+silver_shark+bomb+gold_shark+beast);
-			//System.out.print("下注总数："+totalBet);
-			if (totalBet != 0){//如果有下注
-				Integer single_bet=panelData.getSingle_bet(); //单注额度
-				float totalPrice = 0f; // 计算总额的变量
-				totalPrice =single_bet*totalBet;
-				System.out.println("save总额："+totalPrice);
-				Boolean betCount = false;// 用于判断是否是统计过（默认值为false）
-				panelData.setPanelBetId(StringUtil.createOrderId());// 设置21位的订单号
-				panelData.setCustomer(getLoginCustomer());// 设置所属用户	
-				panelData.setSwallow(swallow);
-				panelData.setPigeon(pigeon);
-				panelData.setPeafowl(peafowl);
-				panelData.setEagle(eagle);
-				panelData.setLion(lion);
-				panelData.setPanda(panda);
-				panelData.setMonkey(monkey);
-				panelData.setRabbit(rabbit);
-				panelData.setBird(bird);
-				panelData.setSilver_shark(silver_shark);
-				panelData.setBomb(bomb);
-				panelData.setGold_shark(gold_shark);
-				panelData.setBeast(beast);
-				panelData.setTotalBet(totalBet);//把下注总数加到对象panelData中
-				panelData.setSingle_bet(single_bet);
-				panelData.setTotalPrice(totalPrice);//把下注总额度
-				panelData.setBetCount(betCount);// 用于判断是否是统计过（默认值为false）
-				panelData.setPrizeItem(Prize.RAFFLING);// 设置奖项状态为正在抽奖
-				betDao.save(panelData);//保存panel获得数据	
-				//记得重置panel下注
-			}
-			//return MAIN;//返回shark主页面
-			return main();//通过主函数返回shark主页面，可进行统计出结果
-		}
-		return CUSTOMER_LOGIN;//返回登入页面
-	}
-	public String uinquirybet() throws Exception{
-
-		return findByCustomer();//返回查询个人下注的方法
-	}
-	/**
-	 * 查询个人下注
-	 * @return String
-	 * @throws Exception
-	 */
-	public String findByCustomer() throws Exception {
-		if(getLoginCustomer() != null){//如果用户已登录
-			String where = "where customer.id = ?";//将用户id设置为查询条件
-			Object[] queryParams = {getLoginCustomer().getId()};//创建对象数组
-			Map<String, String> orderby = new HashMap<String, String>(1);//创建Map集合
-			orderby.put("createTime", "desc");//设置排序条件及方式
-			pageModel = betDao.find(where, queryParams, orderby , pageNo, pageSize);//执行查询方法
-		}
-		return LIST;//返回订单列表页面
-	}
-	
-	/**
-	 * 查询所有人下注
-	 * @return String
-	 * @throws Exception
-	 */
-	public String list() throws Exception {
-		Map<String, String> orderby = new HashMap<String, String>(1);//定义Map集合
-		orderby.put("createTime", "desc");//设置按创建时间倒序排列
-		StringBuffer whereBuffer = new StringBuffer("");//创建字符串对象
-		List<Object> params = new ArrayList<Object>();//创建列表集合
-		if(panelData.getPanelBetId() != null && panelData.getPanelBetId().length() > 0){//如果下注单不为空
-			whereBuffer.append("panelBetId = ?");//以下注单号为查询条件
-			params.add(panelData.getPanelBetId());//设置参数
-		}
-		if(panelData.getCustomer() != null && panelData.getCustomer().getUsername() != null 
-				&& panelData.getCustomer().getUsername().length() > 0){//如果会员名不为空
-			if(params.size() > 0) whereBuffer.append(" and ");//增加查询条件
-			whereBuffer.append("customer.username = ?");//设置会员名为查询条件
-			params.add(panelData.getCustomer().getUsername());//设置参数
-		}
-		//如果whereBuffer为空则查询条件为空，否则以whereBuffer为查询条件
-		String where = whereBuffer.length()>0 ? "where "+whereBuffer.toString() : "";
-		pageModel = betDao.find(where, params.toArray(), orderby, pageNo, pageSize);//执行查询方法
-		System.out.println("最新一单飞禽押注："+pageModel.getList().get(0).getBird());
-		return LIST;//返回后台下注列表
-	}
-	
-	/**
-	 * 查询指定一单的下注情况
-	 */
-	public String select() throws Exception {
-		panelData = betDao.load(panelData.getPanelBetId());
-		return SELECT;
-	}
-	
-	
-	/**
-	 * 转盘页面
-	 * @return
-	 * @throws Exception
-	 */
-	public String gdial() throws Exception{
-
-		return "gdial";
-	}
-	
-	/**
-	 * 按键键盘页面
-	 * @return
-	 * @throws Exception
-	 */
-	public String gpanel() throws Exception{
-
-		return "gpanel";
-	}
-	
-	/**
-	 * 导向grule页面
-	 * @return
-	 * @throws Exception
-	 */
-	public String grule() throws Exception{
-
-		return "grule";
-	}
-	/**
-	 * 游戏shark的设定
-	 * @return shark设定页面
-	 * @throws Exception
-	 */
-	public String option() throws Exception{
-
-		return "option";
-	}
-	
-	// 键盘panel对象
-	private PanelInfo panelData = new PanelInfo();//一定要先初始化obj对象！
-	private PageModel<PanelInfo> pageModel;// 分页组件
-	/**
-	 * 此方法是Modeldriven接口的方法，要重写
-	 * @return
-	 */
-	@Override
-	public PanelInfo getModel() {
-		return panelData;
-	}
-	//getter和setter方法，放入request中，好在jsp页面中能拿到	
-	public PageModel<PanelInfo> getPageModel() {
-		return pageModel;
-	}
-
-	public void setPageModel(PageModel<PanelInfo> pageModel) {
-		this.pageModel = pageModel;
-	}
-/**---------------------------------------------------------------------*/
 	
 }
